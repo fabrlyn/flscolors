1044cbb52785405541a8863334474ac93430d194
fabrlyn <robert.barlin@gmail.com>
2024-02-25 16:13:23 +0100

    Remove todo comment

--- src/bsd.rs
+++ src/bsd.rs
@@ -1,6 +1,5 @@
 // Reference to `ls`and `LSCOLORS`
 // https://man.freebsd.org/cgi/man.cgi?query=ls&apropos=0&sektion=1&manpath=FreeBSD+15.0-CURRENT&arch=default&format=html
-// Look at making some fns const if possible
 
 pub mod attribute;
 pub mod color;
